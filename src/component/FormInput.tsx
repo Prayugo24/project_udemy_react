2e9382e13ee9cdc51de79da97aa4815858173384
prayugo <pray.arsian24@gmai.com>
2022-10-23 01:59:30 +0700

    add style for form input

--- src/component/FormInput.tsx
+++ src/component/FormInput.tsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-
+import Button from "./Button";
+import '../assets/style/FormInput.css';
 
 class FormInput extends Component{
     render(){
         return (
-            <div>
-                form input
-            </div>
+            <form className="formInput">
+                <input type="text" placeholder="Add Text"/>
+                <Button/>
+            </form>
         )
     }
 }
 
 export default FormInput
+
+
